45db7f270b67cbea1cf7f3e7f1fd1b69a9cdfde9
Cecily <its.cecilyy@gmail.com>
2015-05-11 18:25:49 -0700

    Created Own Post

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,16 @@ posts = Post.all
     )
 end
 
+Post.create!(
+    title: "This is me!",
+    body: "This is my post that Cecily created"
+  )
+
+Comment.create!(
+    post: posts.sample,
+    body: "This is a stupid post!"
+  )
+
 puts "Seed Finished"
 puts "#{Post.count} post created"
 puts "#{Comment.count} comments created"
