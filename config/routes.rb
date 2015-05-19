74ef83a86feb6528cb254a80ac59e70db76fca46
Cecily <its.cecilyy@gmail.com>
2015-05-19 14:14:04 -0700

    FixedMergeConflict_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,15 @@
 Bloccit::Application.routes.draw do
-<<<<<<< HEAD
+
   devise_for :users
-  get 'advertisements/index'
-=======
->>>>>>> 3a7bcf7c58c27b99c83ecb36b0cd2137c227d9c2
 
   resources :posts, :questions, :advertisements
 
-<<<<<<< HEAD
-    resources :posts, :advertisements, :questions
-=======
   get 'welcome/contact'
->>>>>>> 3a7bcf7c58c27b99c83ecb36b0cd2137c227d9c2
-
+  
   get 'about' => 'welcome#about'
 
   root to: 'welcome#index'
   
 end
+
 
