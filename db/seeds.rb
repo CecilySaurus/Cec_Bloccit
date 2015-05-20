de6ac66335967fb71b766d4b5c4975ab81f82a68
Cecily <its.cecilyy@gmail.com>
2015-05-20 14:45:49 -0700

    User/Posts_Associaton

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,26 @@
 require 'faker'
 
+ # Create Users
+ 5.times do
+   user = User.new(
+     name:     Faker::Name.name,
+     email:    Faker::Internet.email,
+     password: Faker::Lorem.characters(10)
+   )
+   user.skip_confirmation!
+   user.save!
+ end
+ users = User.all
+ 
+ # Note: by calling `User.new` instead of `create`,
+ # we create an instance of User which isn't immediately saved to the database.
+ 
+ # The `skip_confirmation!` method sets the `confirmed_at` attribute
+ # to avoid triggering an confirmation email when the User is saved.
+ 
+ # The `save` method then saves this User to the database.
+
+
 50.times do
   Advertisement.create!(
     title: Faker::Lorem.sentence,
@@ -12,6 +33,7 @@ advertisements = Advertisement.all
 #Create Posts
 50.times do
   Post.create!(
+    user:   users.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -21,6 +43,7 @@ posts = Post.all
 #Create Comments
 100.times do
   Comment.create!(
+    # user: users.sample,   # we have not yet associated Users with Comments
       post: posts.sample,
       body: Faker::Lorem.paragraph
     )
@@ -42,6 +65,13 @@ Post.create!(
 Comment.create!(
     post: posts.sample,
     body: "This is a stupid post!"
+  )
+
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+    email: 'its.cecilyy@gmail.com',
+    password: 'cecilyc92'
   )
 
 puts "Seed Finished"
