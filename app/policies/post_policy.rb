e4c9641100c719698a3640af79ed0c169e24a658
Cecily <its.cecilyy@gmail.com>
2015-05-24 12:32:16 -0700

    scopesubclass_postpolicy

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,7 +1,25 @@
 class PostPolicy < ApplicationPolicy
-  
+  class Scope < Scope
+    def resolve
+      return scope.none unless user
+
+      if user.moderator? or user.admin?
+        scope.all
+      else
+        scope.where(user_id: user.id)
+      end
+    end
+  end
+
   def index?
     true
   end
 
+  def show?
+    super && (record_owned_by_user? || user_is?('moderator', 'admin'))
+  end
+
+  def destroy?
+    user_exists? && (record_owned_by_user? || user_is?('admin', 'moderator'))
+  end
 end
