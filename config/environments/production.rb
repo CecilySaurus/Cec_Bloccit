8f7a78e34ddd07b2d76c9defa47f8eb4b13d2241
Cecily <its.cecilyy@gmail.com>
2015-05-28 17:19:17 -0700

    Configured app for Production emails.urlfix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,8 +77,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'bloccitsaur.herokuapp.com/' }
+  config.action_mailer.default_url_options = { host: 'bloccitsaur.herokuapp.com' }
 
   # http://bloccit.herokuapp.com
-  config.action_mailer.default_url_options = { host: 'bloccit.herokuapp.com' }
-end
