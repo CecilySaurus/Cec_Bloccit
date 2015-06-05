67c610b0af8e8785b3f352107ef2d204f68a897a
Cecily <its.cecilyy@gmail.com>
2015-06-05 13:18:33 -0700

    mardown_enabledforbody

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,16 @@
 module PostsHelper
+  def markdown_to_html(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
+
+  def markdown_title(title)
+    render_as_markdown
+  end
+
+  def markdown_body(body)
+    render_as_markdown
+  end
 end
